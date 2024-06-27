66bf2c876ccfd2f2a1c5b0608424d5ab87cabb77
Tadiparthi Vamsi <tadiparthivamsi@gmail.com>
2024-06-27 14:43:43 +0530

    revOfLL

---
+++ revOfLL.java
@@ -0,0 +1,19 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+}
