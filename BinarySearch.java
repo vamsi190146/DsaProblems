b252f2c7ef673b98a1b304f89463ec06e1d684cd
Your Name <tadiparthivamsi@gmail.com>
2024-07-05 22:43:07 +0530

    BinarySearch

---
+++ BinarySearch.java
@@ -0,0 +1,32 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11, 13};
+        int target = 7;
+        int index = binarySearch(arr, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
