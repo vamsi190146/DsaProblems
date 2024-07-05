8d9a1aa8ad08837186209714d1a64c773a8e9917
Your Name <tadiparthivamsi@gmail.com>
2024-07-05 22:39:42 +0530

    reverse

---
+++ reverse.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        String reversed = reverseString(str);
+        System.out.println("Original string: " + str);
+        System.out.println("Reversed string: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+}
