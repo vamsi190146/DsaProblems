eb6b8dd861e076c8c67caa10592acf26d598360b
Your Name <tadiparthivamsi@gmail.com>
2024-07-06 08:36:18 +0530

    DupInArr

---
+++ DupInArr.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                ans.add(num);
+            } else {
+                seen.add(num);
+            }
+        }
+        return ans;
+    }
+}
