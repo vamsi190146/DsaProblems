dace5dcb2950a06ef67c3e985305543513786561
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-08-09 08:49:01 +0530

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -5,7 +5,6 @@ public class Solution {
             return result;
         }
         Arrays.sort(nums); 
-
         for (int i = 0; i < nums.length - 2; i++) {
             // Skip duplicate elements to avoid duplicate triplets
             if (i > 0 && nums[i] == nums[i - 1]) {
