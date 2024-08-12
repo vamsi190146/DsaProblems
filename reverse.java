a26e55ee4e298111398782bba7f20c5614a15415
Tadiparthi Vamsi <145857734+vamsi190146@users.noreply.github.com>
2024-08-12 09:25:33 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -6,7 +6,6 @@ public class ReverseString {
         System.out.println("Original string: " + str);
         System.out.println("Reversed string: " + reversed);
     }
-
     public static String reverseString(String str) {
         StringBuilder sb = new StringBuilder(str);
         return sb.reverse().toString();
